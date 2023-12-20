c544b3c14abea0fabb0e299f8738fb7328e68981
njacques <nicholas.d.jacques@gmail.com>
2023-12-19 21:23:25 -0700

    darts

---
+++ darts/darts.rb
@@ -0,0 +1,20 @@
+require 'pry'
+
+class Darts
+    def initialize(x, y)
+        @distance_from_center = Math.sqrt(x**2 + y**2)
+    end
+
+    def score
+        case @distance_from_center
+        when 0..1
+            10
+        when 1..5
+            5
+        when 5..10
+            1
+        else
+            0
+        end
+    end
+end
