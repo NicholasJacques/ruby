cfd9ab69e3ca2f5c2689a2374fa35cd4a659b490
njacques <nicholas.d.jacques@gmail.com>
2023-12-27 16:00:02 -0700

    run_length_encoding_refactor

--- run-length-encoding/run_length_encoding.rb
+++ run-length-encoding/run_length_encoding.rb
@@ -1,6 +1,6 @@
 module RunLengthEncoding
     def self.encode(input)
-        result = []
+        result = ''
         char_count = 0
         input.each_char.with_index do |char, i|
             if input[i+1] != char
@@ -15,13 +15,13 @@ module RunLengthEncoding
                 char_count += 1
             end
         end
-        result.join
+        result
     end
 
     def self.decode(input)
-        result = []
+        result = ''
         current_count = ''
-        input.each_char.with_index do |char, i|
+        input.each_char do |char|
             if char.match?(/[[:digit:]]/)
                 current_count += char
             else
@@ -33,6 +33,6 @@ module RunLengthEncoding
                 end
             end
         end
-        result.join
+        result
     end
 end
