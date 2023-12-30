8db00d7b253b6882cbf33b6a4df8d0981e955a60
njacques <nicholas.d.jacques@gmail.com>
2023-12-29 20:07:26 -0700

    refactor minesweeper

--- minesweeper/minesweeper.rb
+++ minesweeper/minesweeper.rb
@@ -1,55 +1,86 @@
 class Minesweeper
+  def initialize(rows)
+    @map = rows
+  end
 
   def self.annotate(rows)
-    rows.map.with_index do |row, i|
+    new(rows).annotate
+  end
+
+  POSITIONS = [
+    :north_of,
+    :south_of,
+    :east_of,
+    :west_of,
+    :northeast_of,
+    :northwest_of,
+    :southeast_of, 
+    :southwest_of
+  ]
+
+  def annotate
+    @map.map.with_index do |row, y|
       new_row = ''
-      row.each_char.with_index do |char, j|
-        if char == '*'
+      row.each_char.with_index do |char, x|
+        if is_bomb?(char)
           new_row << char
           next
         end
-        neighbors = 0
-        # west
-        if j != 0 && row[j-1] == '*'
-          neighbors += 1
-        end
-        # east
-        if row[j+1] ==  '*'
-          neighbors += 1
-        end
-        # north
-        if i != 0
-          # north-north
-          if rows[i-1][j] == '*'
-            neighbors +=1
-          end
-          #northwest
-          if j != 0 && rows[i-1][j-1] == '*'
-            neighbors +=1
-          end
-          #northeast
-          if rows[i-1][j+1] == '*'
-            neighbors += 1
-          end 
-        end
-        # south
-        if rows[i+1]
-          # south-south
-          if rows[i+1][j] == '*'
-            neighbors += 1
-          end
-          # southwest
-          if j != 0 && rows[i+1][j-1] == '*'
-            neighbors += 1
-          end
-          #southeast
-          if rows[i+1][j+1] == '*'
-            neighbors += 1
-          end
-        end
+        neighbors = POSITIONS.count {|position| is_bomb?(send(position, x, y))}
         neighbors > 0 ? new_row << neighbors.to_s : new_row << ' '
       end
       new_row
     end
   end
+
+  def west_of(x, y)
+    if x != 0
+      @map[y][x-1]
+    end
+  end
+
+  def east_of(x, y)
+    @map[y][x+1]
+  end
+
+  def north_of(x, y)
+    if @map[y-1] && y != 0
+      @map[y-1][x]
+    end
+  end
+
+  def south_of(x, y)
+    if @map[y+1]
+      @map[y+1][x]
+    end
+  end
+
+  def northeast_of(x, y)
+    if @map[y-1] && y != 0
+      @map[y-1][x+1]
+    end
+  end
+
+  def northwest_of(x, y)
+    if @map[y-1] && x != 0 && y != 0
+      @map[y-1][x-1]
+    end
+  end
+
+  def southeast_of(x, y)
+    if @map[y+1]
+      @map[y+1][x+1]
+    end
+  end
+
+  def southwest_of(x, y)
+    if @map[y+1] && x != 0
+      @map[y+1][x-1]
+    end
+  end
+
+  def is_bomb?(char)
+    char == '*'
+  end
+
 end
