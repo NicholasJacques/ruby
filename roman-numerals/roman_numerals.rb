9422da9610136bacaebee499d1ab51ba4fa34fda
njacques <nicholas.d.jacques@gmail.com>
2024-01-04 17:12:20 -0700

    cleanup

--- roman-numerals/roman_numerals.rb
+++ roman-numerals/roman_numerals.rb
@@ -2,22 +2,22 @@ class Integer
   ROMAN_NUMERAL_SYMBOLS = %w(I V X L C D M)
 
   def to_roman
-    result = []
+    result = ""
     current_magnitude = 0
     self.digits.each do |digit|
       if (1..3).include?(digit)
-        result << ROMAN_NUMERAL_SYMBOLS[current_magnitude] * digit
+        result.prepend(ROMAN_NUMERAL_SYMBOLS[current_magnitude] * digit)
       elsif digit == 4
-        result << ROMAN_NUMERAL_SYMBOLS[current_magnitude] + ROMAN_NUMERAL_SYMBOLS[current_magnitude + 1]
+        result.prepend(ROMAN_NUMERAL_SYMBOLS[current_magnitude] + ROMAN_NUMERAL_SYMBOLS[current_magnitude + 1])
       elsif digit == 5
-        result << ROMAN_NUMERAL_SYMBOLS[current_magnitude + 1]
+        result.prepend(ROMAN_NUMERAL_SYMBOLS[current_magnitude + 1])
       elsif (6..8).include?(digit)
-        result << ROMAN_NUMERAL_SYMBOLS[current_magnitude + 1] + ROMAN_NUMERAL_SYMBOLS[current_magnitude] * (digit - 5)
+        result.prepend(ROMAN_NUMERAL_SYMBOLS[current_magnitude + 1] + ROMAN_NUMERAL_SYMBOLS[current_magnitude] * (digit - 5))
       elsif digit == 9
-        result << ROMAN_NUMERAL_SYMBOLS[current_magnitude] + ROMAN_NUMERAL_SYMBOLS[current_magnitude + 2]
+        result.prepend(ROMAN_NUMERAL_SYMBOLS[current_magnitude] + ROMAN_NUMERAL_SYMBOLS[current_magnitude + 2])
       end
       current_magnitude += 2
     end
-    result.reverse.join
+    result
   end
 end
