d05857d6e883bb13f580decc900331f03bf70090
njacques <nicholas.d.jacques@gmail.com>
2024-01-05 14:13:21 -0700

    more easy ones

---
+++ difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,18 @@
+class Squares
+  def initialize(number)
+    @number = number
+  end
+
+  def square_of_sum
+    (0..@number).sum**2
+  end
+
+  def sum_of_squares
+    (0..@number).reduce {|sum, n| sum + n**2 }
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
+  end
+
+end
